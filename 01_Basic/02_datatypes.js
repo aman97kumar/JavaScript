1dd1569658a47ba0ed351a5ee382ed066465a123
Aman kumar <118106497+aman97kumar@users.noreply.github.com>
2025-02-17 00:03:29 +0000

    datatypes in js

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict";// treat all js code as newer version.
+
+// alert(3+3); alert is not defined, because we are using nodejs not browser.
+
+// code readability should be high.
+
+// mdn documentaion read.
+// ecma js standard documetation.
+
+let name = "aman"; // string
+
+let age = 20; // number
+
+let isLoggedIn = false; // boolean
+
+// datatypes in js ->
+// number => 2 to power 53 limit.
+// bigint => use if number is much much big.
+// string => use '' or "" both are correct.
+// boolean => true/false.
+// null => standalone value. ex-use in temp if error from server.
+// undefined => let num;
+// symbol => for uniquness,components use in react mostly;
+
+// object => study in depth
+
+console.log(typeof(null)); // object
+console.log(typeof(undefined)); // undefined
+console.log(typeof(symbol));
+
+
+
