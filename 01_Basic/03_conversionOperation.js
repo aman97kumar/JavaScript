636847d1cfe65275fd5d9f5e6b57482f1fe90fa4
Aman kumar <118106497+aman97kumar@users.noreply.github.com>
2025-02-17 08:48:51 +0000

    add other topic

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -56,3 +56,65 @@ let someNumber = 33; // number
 let stringNumber = String(someNumber); // coversion number to string
 console.log(stringNumber); // 33
 console.log(typeof(stringNumber)); // string
+
+
+
+// some other js topic
+
+let str1 = "aman"
+let str2 = " kumar"
+let str3 = str1+str2; 
+console.log(str3) // aman kumar
+
+console.log(2**3) // 2 to power 3
+console.log("1"+2) // 12
+console.log(1+"2") // 12
+console.log("1"+2+2) // 122
+console.log(1+2+"2") // 32
+console.log(+true) // 1
+console.log(+"") // 0
+
+
+//don't write these type of code
+let num1, num2, num3
+num1=num2=num3=4
+
+let gameCounter=0
+gameCounter++ // postfix
+console.log(gameCounter) // 1
+
+let x = 3;
+const y = x++; // postfix first assign then increment
+console.log(y) // 3
+console.log(x) // 4
+
+
+let w = 3;
+const z = ++w; // prefix first increment then assign
+console.log(z) //4
+console.log(w) //4
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`); // Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`); // Expected output: "a:4, b:4"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
